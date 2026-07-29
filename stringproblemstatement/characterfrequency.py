cc154117e238af65d7ecbd3e5b7987d8e150026b
Manasi Sargar <sargarmanasi001@gmail.com>
2026-07-29 21:52:16 +0530

    Delete stringproblemstatement directory

--- stringproblemstatement/characterfrequency.py
+++
@@ -1,5 +0,0 @@
-s = input("Enter a string: ")
-
-for ch in s:
-    if s.index(ch) == s.find(ch):
-        print(ch, ":", s.count(ch))
--- stringproblemstatement/replacecharacter.py
+++
@@ -1,7 +0,0 @@
-s = input("Enter a string: ")
-old = input("Enter character to replace: ")
-new = input("Enter new character: ")
-
-result = s.replace(old, new)
-
-print("New string:", result)
