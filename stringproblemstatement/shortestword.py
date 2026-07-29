4e7f3aa75334dfcf70df9556400f43e073863fd0
Manasi Sargar <sargarmanasi001@gmail.com>
2026-07-29 21:44:32 +0530

    Add files via upload

---
+++ stringproblemstatement/shortestword.py
@@ -0,0 +1,10 @@
+s = input("Enter a sentence: ")
+
+words = s.split()
+shortest = words[0]
+
+for word in words:
+    if len(word) < len(shortest):
+        shortest = word
+
+print("Shortest word:", shortest)
